bed16bc16bdca436856940d711e2099efe075993
umer786555 <140739681+umer786555@users.noreply.github.com>
2025-04-17 16:03:43 +0100

    chore: added equatable

--- lib/features/all_launches/bloc/all_launches_state.dart
+++ lib/features/all_launches/bloc/all_launches_state.dart
@@ -1,20 +1,38 @@
 part of 'all_launches_bloc.dart';
 
 @immutable
-sealed class AllLaunchesState {}
+sealed class AllLaunchesState extends Equatable {}
 
-final class AllLaunchesInitial extends AllLaunchesState {}
+final class AllLaunchesInitial extends AllLaunchesState {
+  @override
+  // TODO: implement props
+  List<Object?> get props => [];
+}
 
-final class AllLaunchesLoading extends AllLaunchesState {}
+final class AllLaunchesLoading extends AllLaunchesState {
+  @override
+  List<Object?> get props => [];
+}
 
 final class AllLaunchesLoaded extends AllLaunchesState {
   final List<LaunchModel> launches;
 
   AllLaunchesLoaded({required this.launches});
+
+  @override
+  List<Object?> get props => [launches];
 }
 
 final class AllLaunchesError extends AllLaunchesState {
   final String message;
 
   AllLaunchesError({required this.message});
+
+  @override
+  List<Object?> get props => [message];
+}
+
+final class AllLaunchesEmpty extends AllLaunchesState {
+  @override
+  List<Object?> get props => [];
 }
