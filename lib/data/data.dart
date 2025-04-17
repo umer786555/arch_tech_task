40c39dbe41f95cfb1fbee09894e6b4d56281d66f
umer786555 <140739681+umer786555@users.noreply.github.com>
2025-04-17 15:59:49 +0100

    feat: put in try catch

--- lib/data/data.dart
+++ lib/data/data.dart
@@ -1,13 +1,23 @@
 import 'package:http/http.dart' as http;
 
 class DataService {
-  Future<http.Response> get(String endpoint) async {
-    final response = await http.get(Uri.parse(endpoint));
+  Future<http.Response> get(String endpoint,
+      {Map<String, String>? headers}) async {
+    try {
+      final response = await http.get(
+        Uri.parse(endpoint),
+        headers: headers,
+      );
 
-    if (response.statusCode == 200) {
-      return response;
-    } else {
-      throw Exception('Failed to load data');
+      if (response.statusCode == 200) {
+        return response;
+      } else {
+        throw Exception('Failed to load data: ${response.statusCode}');
+      }
+    } catch (e) {
+      throw Exception('Error fetching data: $e');
     }
   }
+
+  // TODO : add post, put, delete methods if needed
 }
